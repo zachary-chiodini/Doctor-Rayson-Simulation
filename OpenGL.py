909598bbddc0badd7417f5fb3533dc9056b3b51b
zachary-chiodini <zacharybchiodini@gmail.com>
2024-05-27 05:17:40 -0400

    edit

--- OpenGL.py
+++ OpenGL.py
@@ -20,13 +20,13 @@ class Window:
             raise RuntimeError('GLFW could not be initialized.')
         self.instantiated = True
         monitor = self.Monitor()
-        self.context = glfw.create_window(
+        self._glfw_window = glfw.create_window(
             *monitor.resolution, monitor.name, monitor.object, None)
-        if not self.context:
+        if not self._glfw_window:
             self.close()
             raise RuntimeError('GLFW window could not be created')
-        glfw.set_key_callback(self.context, self.key_callback)
-        glfw.make_context_current(self.context)
+        glfw.set_key_callback(self._glfw_window, self.key_callback)
+        glfw.make_context_current(self._glfw_window)
 
     def close(self) -> None:
         if self.instantiated:
@@ -34,14 +34,14 @@ class Window:
         return None
 
     def is_open(self) -> int:
-        return 1 - glfw.window_should_close(self.context)
+        return 1 - glfw.window_should_close(self._glfw_window)
 
     @staticmethod
-    def key_callback(window, key, scancode, action, mods) -> None:
+    def key_callback(glfw_window, key, scancode, action, mods) -> None:
         if (key == glfw.KEY_ESCAPE) and (action == glfw.PRESS):
-            glfw.set_window_should_close(window, glfw.TRUE)
+            glfw.set_window_should_close(glfw_window, glfw.TRUE)
 
     def refresh(self) -> None:
-        glfw.swap_buffers(self.context)
+        glfw.swap_buffers(self._glfw_window)
         glfw.poll_events()
         return None
