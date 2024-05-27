15f2f828fa7dc91d805b33419a94e01f40344be2
zachary-chiodini <zacharybchiodini@gmail.com>
2024-05-27 06:55:12 -0400

    edit

--- OpenGL.py
+++ OpenGL.py
@@ -13,35 +13,34 @@ class Window:
             self.resolution = video_mode.size
             self.refresh_rate = video_mode.refresh_rate
 
-    instantiated = False
-
     def __init__(self):
-        if not glfw.init():
+        self._glfw_init = glfw.init()
+        if not self._glfw_init:
             raise RuntimeError('GLFW could not be initialized.')
-        self.instantiated = True
         monitor = self.Monitor()
         self._glfw_window = glfw.create_window(
             *monitor.resolution, monitor.name, monitor.object, None)
         if not self._glfw_window:
-            self.close()
+            self._close()
             raise RuntimeError('GLFW window could not be created')
-        glfw.set_key_callback(self._glfw_window, self.key_callback)
+        glfw.set_key_callback(self._glfw_window, self._key_callback)
         glfw.make_context_current(self._glfw_window)
 
-    def close(self) -> None:
-        if self.instantiated:
-            glfw.terminate()
-        return None
-
     def is_open(self) -> int:
-        return 1 - glfw.window_should_close(self._glfw_window)
-
-    @staticmethod
-    def key_callback(glfw_window, key, scancode, action, mods) -> None:
-        if (key == glfw.KEY_ESCAPE) and (action == glfw.PRESS):
-            glfw.set_window_should_close(glfw_window, glfw.TRUE)
+        return self._glfw_init & (1 - glfw.window_should_close(self._glfw_window)) or self._close()
 
     def refresh(self) -> None:
         glfw.swap_buffers(self._glfw_window)
         glfw.poll_events()
         return None
+
+    def _close(self) -> int:
+        if self._glfw_init:
+            glfw.terminate()
+            self._glfw_init = 0
+        return 0
+
+    @staticmethod
+    def _key_callback(glfw_window, key, scancode, action, mods) -> None:
+        if (key == glfw.KEY_ESCAPE) & (action == glfw.PRESS):
+            glfw.set_window_should_close(glfw_window, glfw.TRUE)
